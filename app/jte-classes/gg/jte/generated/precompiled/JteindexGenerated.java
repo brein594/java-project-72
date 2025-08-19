23aec7e7e1694c76c3f4f180d25d4c26f54f7508
brein594 <brein594@gmail.com>
2025-08-19 19:55:44 +0300

    update makefile. Add check

---
+++ app/jte-classes/gg/jte/generated/precompiled/JteindexGenerated.java
@@ -0,0 +1,11 @@
+package gg.jte.generated.precompiled;
+public final class JteindexGenerated {
+	public static final String JTE_NAME = "index.jte";
+	public static final int[] JTE_LINE_INFO = {11,11,11,11,11,11,11,11,11,11,11};
+	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor) {
+		jteOutput.writeContent("<!doctype html>\n<html lang =\"ru\">\n<head>\n    <meta charset=\"utf-8\">\n    <title>Hello Hexlet</title>\n</head>\n<body>\n    <main>\n        <h2>Hello World!</h2>\n    </main>\n</body>\n</html>");
+	}
+	public static void renderMap(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, java.util.Map<String, Object> params) {
+		render(jteOutput, jteHtmlInterceptor);
+	}
+}
